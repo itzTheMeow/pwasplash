f77143c5da56dafe23dd80bbed23afdaa495ec26
Meow <me@itsmeow.cat>
2023-04-19 22:01:18 -0400

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# typescript-template
-A template for TypeScript repositories.
+# pwasplash
+
+A small dependency-free utility to generate splash images for iOS PWAs automatically. This does not generate app icons, as those are easier to do yourself.
+
+Supports a few custom options for optimal image generation.
+
+This is inspired by [pwacompat](https://www.npmjs.com/package/pwacompat), but implements it way better while sacrificing some browser support. (IE is dead)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-// remember to fill out package.json
+/**
+ * Generates a splash image for the current document.
+ * @param logoPath Loaded image element, canvas, or the URL of an image to load.
+ * @param options Options for splash generation.
+ * @param options.background Background color for the splash image. (defaults to the computed background-color of the `body` tag)
+ * @param options.scale Scale of the icon relative to the background. (1 is full-width, 3 is 1/3 width)
+ */
+export default async function generateSplash(
+  logoPath: HTMLImageElement | HTMLCanvasElement | string,
+  options?: {
+    background?: string;
+    scale?: number;
+  }
+) {
+  const icon =
+      typeof logoPath == "string"
+        ? await new Promise<HTMLImageElement>((r) => {
+            const img = new Image();
+            img.onload = () => r(img);
+            img.src = logoPath;
+          })
+        : logoPath,
+    { width, height } = window.screen,
+    // optimal logo size based on options
+    logoSize = Math.min(icon.width, icon.height) / (options?.scale || 3),
+    ratio = window.devicePixelRatio,
+    canvas = document.createElement("canvas");
+  canvas.width = width * ratio;
+  canvas.height = height * ratio;
+
+  const ctx = canvas.getContext("2d")!;
+  ctx.scale(ratio, ratio);
+  ctx.fillStyle =
+    options?.background || window.getComputedStyle(document.body).backgroundColor || "#000000";
+  ctx.fillRect(0, 0, width, height);
+
+  // draw image in the center of the splash
+  ctx.drawImage(icon, width / 2 - logoSize / 2, height / 2 - logoSize / 2, logoSize, logoSize);
+
+  const link = document.createElement("link");
+  link.setAttribute("rel", "apple-touch-startup-image");
+  link.setAttribute("href", canvas.toDataURL());
+  document.head.appendChild(link);
+}
